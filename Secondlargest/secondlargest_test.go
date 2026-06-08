6ad89a77ce38522097d268127832f0d9208e86e2
Sam <vasanthgoud2001@gmail.com>
2026-06-08 19:06:22 +0530

    added edge cases

--- Secondlargest/secondlargest_test.go
+++ Secondlargest/secondlargest_test.go
@@ -12,3 +12,39 @@ func TestSecondLargest(t*testing.T){
 		t.Errorf("Expected %d,but got %d", Expected, result)
 	}
 }
+
+func TestSecondLargestNegative(t*testing.T) {
+		num := []int{-23,-56,56,87,-98}
+
+		result := secondLargest(num)
+
+		Expected := 56
+
+	if result != 56 {
+		t.Errorf("Expected %d, got %d", Expected, result)
+	}
+}
+
+func TestSingleElementArray(t*testing.T) {
+		num := []int{24}
+
+		result := secondLargest(num)
+
+		Expected := 24
+
+	if result != 24 {
+		t.Errorf("Expected %d, got %d", Expected, result)
+	}
+}
+
+func TestDuplicateLargest(t*testing.T) {
+		num := []int{24,56,66,45,66}
+
+		result := secondLargest(num)
+
+		Expected := 56
+
+	if result != 56 {
+		t.Errorf("Expected %d, got %d", Expected, result)
+	}
+}
