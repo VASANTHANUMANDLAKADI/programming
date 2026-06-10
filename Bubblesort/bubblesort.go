aa3bf0221280a48b1a37ad55fd5ffae8471fb3a0
Sam <vasanthgoud2001@gmail.com>
2026-06-10 17:09:31 +0530

    Made changes as per comments in PR

--- Bubblesort/bubblesort.go
+++ Bubblesort/bubblesort.go
@@ -3,10 +3,10 @@ package main
 func bubbleSort(array []int)[]int{
 	n := len(array)
 
-	for i:= 0; i < n-1; i++ {
-		for j:= 0; j< n-1-i; j++{
-			if array[j] > array[j+1]{
-				array[j], array[j+1] = array[j+1], array[j]
+	for i:= 0; i < n-1; i++ {		// This loop will iterate as per length of array
+		for j:= 0; j< n-1-i; j++{	
+			if array[j] > array[j+1]{  // Comparesion of elements in array
+				array[j], array[j+1] = array[j+1], array[j] // Sorting of elements in array 
 			}
 		}
 	}
--- Bubblesort/bubblesort_test.go
+++ Bubblesort/bubblesort_test.go
@@ -2,16 +2,40 @@ package main
 
 import (
 		"testing"
-		"reflect"
-)
+		"github.com/stretchr/testify/assert"
+	)
 func TestBubbleSort(t *testing.T){
 
 	array := []int {66,44,75,2,29,20,24}
 	expected := []int {2,20,24,29,44,66,75}
 
-	array = bubbleSort(array) 
+	assert.Equal(t, expected, bubbleSort(array))
+	
+}
 
-	if !reflect.DeepEqual(array,expected) {
-		t.Errorf("Expected %v but got %v", expected,array)
-	}
+func TestAlreadysorted(t *testing.T){
+
+	array := []int {1,2,3,4,5,6}
+	expected := []int {1,2,3,4,5,6}
+
+	assert.Equal(t, expected, bubbleSort(array))
+	
+}
+
+func TestReverseSorted(t *testing.T){
+
+	array := []int {75,66,44,29,24,20,2}
+	expected := []int {2,20,24,29,44,66,75}
+
+	assert.Equal(t, expected, bubbleSort(array))
+	
+}
+
+func TestDuplicateElements(t *testing.T){
+
+	array := []int {66,24,75,2,29,66,24}
+	expected := []int {2,24,24,29,66,66,75}
+
+	assert.Equal(t, expected, bubbleSort(array))
+	
 }
