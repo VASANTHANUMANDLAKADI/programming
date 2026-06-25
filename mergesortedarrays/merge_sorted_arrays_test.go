4fa55739714ae24a023ff9a1c14bf55548277d42
Sam <vasanthgoud2001@gmail.com>
2026-06-25 12:49:32 +0530

    Created a merge for sorted arrays

---
+++ mergesortedarrays/merge_sorted_arrays_test.go
@@ -0,0 +1,18 @@
+package merge
+import (
+	"testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestMergesortedarrays(t *testing.T){
+	a := []int{14,22,24,56,99}
+	b := []int{16,36,67,84,100}
+
+	expected:= []int{14,16,22,24,36,56,67,84,99,100}
+
+	assert.Equal(t, expected, mergesortedarrays(a,b))
+}
+
+func mergesortedarrays(a []int, b []int) interface{} {
+	panic("unimplemented")
+}
