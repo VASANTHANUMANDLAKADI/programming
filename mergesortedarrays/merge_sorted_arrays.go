be37da919c5849c2bf655408c6cae8c385d56bf5
Sam <vasanthgoud2001@gmail.com>
2026-06-26 11:15:36 +0530

    Merge

--- mergesortedarrays/merge_sorted_arrays.go
+++ mergesortedarrays/merge_sorted_arrays.go
@@ -1,25 +1,32 @@
 package merge
 
-func mergesortedarrays(a,b []int)[]int{
-	result:= []int{}
-	i:= 0
-	j:= 0
+func mergesortedarrays(a, b []int) []int {
+	result := []int{}
+	i := 0
+	j := 0
 
-	for i<len(a)&& j<len(b){
-		if a[i]<b[j]{
-			result = append(result,a[i])
+	// Compare elements from both arrays
+	for i < len(a) && j < len(b) {
+		if a[i] <= b[j] {
+			result = append(result, a[i])
 			i++
-		}else{
-			result = append(result,b[j])
+		} else {
+			result = append(result, b[j])
+			j++
 		}
 	}
-	for i<len(a){
-		result = append(result,a[i])
+
+	// Add remaining elements from array a
+	for i < len(a) {
+		result = append(result, a[i])
 		i++
 	}
-	for j<len(b){
-		result = append(result,b[j])
+
+	// Add remaining elements from array b
+	for j < len(b) {
+		result = append(result, b[j])
 		j++
 	}
+
 	return result
 }
