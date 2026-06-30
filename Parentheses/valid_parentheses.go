e67e124a2e99a2e57706d62a77335fa7180387ba
Sam <vasanthgoud2001@gmail.com>
2026-06-30 13:23:05 +0530

    Add valid parentheses solution

---
+++ Parentheses/valid_parentheses.go
@@ -0,0 +1,31 @@
+package parentheses
+
+func isBalanced(s string)bool{
+	stack:= []rune{}
+	
+	pairs:= map[rune]rune{
+		')' : '(',
+		'}' : '{',
+		']' : '[',
+	}
+	for _, ch := range s {
+		switch ch {
+		case '(', '{', '[':
+			stack = append(stack, ch)
+
+		case ')', '}', ']':
+			if len(stack) == 0{
+				return false
+			}
+			 top:= stack[len(stack)-1]
+			 stack = stack[:len(stack)-1]
+
+			 if top != pairs[ch]{
+				return false
+			 }
+		}
+	}
+	return len(stack) == 0
+}
+
+
