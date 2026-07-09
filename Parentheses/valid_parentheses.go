4c9698035476bf03145833a1e2db5294866ea6fa
Sam <vasanthgoud2001@gmail.com>
2026-07-09 11:45:54 +0530

    Made some changes

--- Parentheses/valid_parentheses.go
+++ Parentheses/valid_parentheses.go
@@ -1,31 +1,59 @@
 package parentheses
 
-func isBalanced(s string)bool{
-	stack:= []rune{}
+//s = []
+func isBalanced(str string)bool{
+	stack:= []string{}
 	
-	pairs:= map[rune]rune{
-		')' : '(',
-		'}' : '{',
-		']' : '[',
+	// if len(s) %2 != 0 {
+	// 	return false
+	// }
+	pairs:= map[string]string{
+		")" : "(",
+		"}" : "{",
+		"]" : "[",
 	}
-	for _, ch := range s {
-		switch ch {
-		case '(', '{', '[':
-			stack = append(stack, ch)
+	for _, ch := range str {
+		s := string(ch)
+		switch s {
+		case "(", "{", "[":
+			stack = append(stack, s) //stack = [
 
-		case ')', '}', ']':
+		case ")", "}", "]":
 			if len(stack) == 0{
 				return false
 			}
-			 top:= stack[len(stack)-1]
-			 stack = stack[:len(stack)-1]
+			 top:= stack[len(stack)-1] //top = 
+			 stack = stack[:len(stack)-1] //empty
 
-			 if top != pairs[ch]{
+			 if top != pairs[s]{
 				return false
 			 }
 		}
 	}
 	return len(stack) == 0
+}
+
+func isBalanced2(s string) bool {
+
+	pairs:= map[string]string{
+		")" : "(",
+		"}" : "{",
+		"]" : "[",
+	}
+
+	//mid := len(s) /2
+	for i := 0; i < len(s) /2; i++ {
+		
+		lastIndex := (len(s) -i) -1
+
+		lastChar := string(s[lastIndex])
+		firstChar := string(s[i])
+		if firstChar != pairs[lastChar] {
+			return false
+		}
+	}
+   
+	return true
 }
 
 
--- Parentheses/valid_parentheses_test.go
+++ Parentheses/valid_parentheses_test.go
@@ -7,23 +7,45 @@ import (
 func TestVaildParentheses(t* testing.T){
 	a:= "[{()}]"
 
-	assert.Equal(t, true, isBalanced(a))
+	assert.Equal(t, true, isBalanced2(a))
 }
 
 func TestEmptyString(t* testing.T){
 	a:= ""
 
-	assert.Equal(t, true, isBalanced(a))
+	assert.Equal(t, true, isBalanced2(a))
 }
 
 func TestIncorrectOrder(t* testing.T){
 	a:= "[({)]}"
 
-	assert.Equal(t, false, isBalanced(a))
+	assert.Equal(t, false, isBalanced2(a))
 }
 
 func TestMissingParentheses(t* testing.T){
 	a:= "[{(]"
 
-	assert.Equal(t, false, isBalanced(a))
+	assert.Equal(t, false, isBalanced2(a))
+}
+
+func TestMissingParentheses_single(t* testing.T){
+	a:= "[{}]"
+	
+	// split 
+	// s1 = "[{"
+	// s2 = "}]"
+	//counter = len(s2), counter = conter -1
+	//pairs [<===>], {<==>}
+
+	//loop s1 
+	//1st -> [
+	   //check it's pair, its is ]
+	   //get last value from s2, s2[1] == ]
+	   //if s1[1st] != s2[last] return false
+	//2nd -> {
+	  //check it's pair, its is }
+	   //get last second value from s2, s2[1] == ]
+	   //if s1[1st] != s2[last] return false
+	  // counter = counter -1
+	assert.Equal(t, true, isBalanced2(a))
 }
